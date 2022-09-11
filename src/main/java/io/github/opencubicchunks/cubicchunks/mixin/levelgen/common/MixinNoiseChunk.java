9f6ba875faee73ff7a4c176311cf95bdd674c648
BelgianSalamander <salamanderhash0514@gmail.com>
2022-09-11 18:03:59 +1200

    Fix checkstyle :D

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/levelgen/common/MixinNoiseChunk.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/levelgen/common/MixinNoiseChunk.java
@@ -14,15 +14,11 @@ import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.NoiseRouter;
 import net.minecraft.world.level.levelgen.NoiseSettings;
 import net.minecraft.world.level.levelgen.blending.Blender;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NoiseChunk.class)
 public abstract class MixinNoiseChunk {
